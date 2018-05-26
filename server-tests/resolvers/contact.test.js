fd5f23737a2ab0dab8aa961f28174c43eaad0bc2
kevinccbsg <kevinccbsg@gmail.com>
2018-05-26 11:13:30 -0400

    Testing  - Added test for mutation  - new contact

--- server-tests/resolvers/contact.test.js
+++ server-tests/resolvers/contact.test.js
@@ -1,6 +1,6 @@
 const { graphql } = require('graphql');
 const schema = require('./../../graphql/schema');
-const { contacts } = require('../fixtures/contact-fixture');
+const { contacts, newContact } = require('../fixtures/contact-fixture');
 
 describe('Contact testing', () => {
   it('Contact list', async () => {
@@ -21,5 +21,21 @@ describe('Contact testing', () => {
     const result = await graphql(schema, query, null, { user: 'test' });
     const results = result.data.contacts;
     expect(results).toEqual(contacts);
+  });
+
+  it('New Contact', async () => {
+    const mutation = `
+      mutation createContact($newContact: NewContact){
+        createContact(contact: $newContact) {
+          name
+          lastName
+          phone
+        }
+      }
+    `;
+    const result = await graphql(schema, mutation, null, { newContact });
+    console.log(result);
+    const results = result.data.contact;
+    expect(results).toEqual(newContact);
   });
 });
