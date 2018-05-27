319b0b67986346df7ab1f1974ee0fc44afeb968d
kevinccbsg <kevinccbsg@gmail.com>
2018-05-27 16:14:58 -0400

    GraphQL  - Added apollo provider

--- src/client.js
+++ src/client.js
@@ -2,29 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
 import 'semantic-ui-css/semantic.min.css';
 import './styles/style.styl';
 import App from './App';
 
 const client = new ApolloClient();
 
-client
-  .query({
-    query: gql`{
-      contacts {
-        name
-        lastName
-        phone
-        companyName
-        address
-        country
-        city
-      }
-    }`
-  })
-  .then(result => console.log(result));
-
-
 render((
-  <App />
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 ), document.getElementById('root'));
